7553626cd191ad5d1e4db228d399254d424efa7f
Jared Silver <jaredsilver@Jareds-MacBook-Pro.local>
2022-02-10 10:04:31 -0800

    github finder app complete

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from 'react'
+import gitHubReducer from './GithubReducer'
+
+const GithubContext = createContext()
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  }
+
+  const [state, dispatch] = useReducer(gitHubReducer, initialState)
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export default GithubContext
